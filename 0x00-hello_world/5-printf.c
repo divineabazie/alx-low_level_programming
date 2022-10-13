62d3cd645f4207d31282a34160b1dd85ddee59a4
Divine Abazie <divineabazie14@gmail.com>
2022-10-13 14:52:24 -0700

    parentheses

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 /**
- *  *main -  Prints exactly "with proper grammar, but the outcome is a piece of art,
- *  " followed by a new line.
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
  *
- *  Return: Always 0.
+ * Return: Always 0.
  */
 int main(void)
-	{	
-		printf	("with proper grammar, but the outcome is a piece of art,\n");
-											
-		return(0);
+{
+		printf("with proper grammar,
+				but the outcome is a piece of art,\n")
+
+		return (0)
+
+
 }
