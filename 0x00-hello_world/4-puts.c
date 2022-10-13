b4c2c612274e00b8dc444304f51cb91f4294bf6a
Divine Abazie <divineabazie14@gmail.com>
2022-10-13 09:52:55 -0700

    betty abeg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,27 @@
-#!/bin/bash
-{
-	puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
 
-	return (0);
+
+
+/**
+ *
+ *  * main - Prints "Programming is like building a multilingual
+ *
+ *   *                puzzle, followed by a new line.
+ *
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
 
 }
