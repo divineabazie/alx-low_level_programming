6981445a222073badcd30c79650fa9c2fc45cad5
Divine Abazie <divineabazie14@gmail.com>
2022-10-15 13:02:51 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Return: Always 
+ * Return: Always
  */
 
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
-	}	
+	}
 
 	putchar('\n');
 
