0c1fe8f38f189eb275297b464d5ef96616510594
Divine Abazie <divineabazie14@gmail.com>
2022-10-15 15:41:01 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,16 +9,15 @@
 int main(void)
 {
 	char c;
-	{
+
 	for (c = 'a'; c <= 'z'; c++)
-	}
-
-	if (c == 'q' || c == 'e')
 	{
-		continue;
-	}
-		putchar(c);
+	if (c == 'q' || c == 'e')
 
+		continue;
+
+		putchar(c);
+	}
 	putchar ('\n');
 
 		return (0);
