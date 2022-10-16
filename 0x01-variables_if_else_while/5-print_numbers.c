04fa6c447c8baed56c7991079b50b3f0ecac2148
Divine Abazie <divineabazie14@gmail.com>
2022-10-15 23:35:48 -0700

    loop single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	int x;
+
+	x = n % 10;
+
+	for (x = 9; x <= 9; x++)
+	{
+	printf("%d", x);
+	}
+
+	return (0);
+
+
+
+
+
+
+}
