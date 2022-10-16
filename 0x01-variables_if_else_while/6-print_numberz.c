2291de1737910a902124dc017cec81c9f1aba0e5
Divine Abazie <divineabazie14@gmail.com>
2022-10-16 00:20:15 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+
+		putchar('\n');
+
+	return (0);
+
+}
