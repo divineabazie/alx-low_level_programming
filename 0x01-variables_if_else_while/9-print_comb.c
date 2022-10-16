968a6149f7364590ec277b77102de7e995e9633f
Divine Abazie <divineabazie14@gmail.com>
2022-10-16 04:16:56 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,15 @@ int main(void)
 
 	x = 0;
 	for (x = '0'; x <= '9'; ++x)
+		putchar(x);
 
-	if (x < '9')
+	if (x == '9')
 	{
 		putchar(',');
-		putchar(x);
+		putchar(' ');
 	}
+
+	putchar ('\n');
 
 	return (0);
 
