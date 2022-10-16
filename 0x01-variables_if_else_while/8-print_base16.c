d9d7d9ab8e6ec417ced3c2e66c51e69a9bc92849
Divine Abazie <divineabazie14@gmail.com>
2022-10-16 02:08:47 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	x = 0;
+	for (x = 0; x <= 10; ++x)
+		putchar(x);
+
+	y = 'a';
+	for (y = 'a'; y <= 'f'; ++y)
+		putchar(y);
+
+	putchar('\n');
+
+		return (0);
+
+
+
+}
