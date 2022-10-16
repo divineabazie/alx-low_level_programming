762879ba76ecba510e7e49782c147e475596168b
Divine Abazie <divineabazie14@gmail.com>
2022-10-16 02:57:05 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	for (x = '0'; x <= '9'; ++x)
+		putchar(x);
+
+	if (x < '9')
+		putchar(',');
+
+			putchar('\n');
+
+				return (0);
+
+
+}
