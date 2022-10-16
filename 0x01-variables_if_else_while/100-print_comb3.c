16ea3bbde62bc6ce8c579231da1ff7cbb0415c7b
Divine Abazie <divineabazie14@gmail.com>
2022-10-16 07:30:10 -0700

    0-positive_or_negative

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+	for (y = '0'; y <= '9'; y++)
+	}
+	{
+	if (x ! = y)
+	}
+	putchar(x, y)
+
+	putchar('\n')
+
+	return (0)
+
+}
