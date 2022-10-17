fc2c23fa8b7da136f1bc1c105c31c17cee91df57
Divine Abazie <divineabazie14@gmail.com>
2022-10-17 03:17:30 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,15 +16,19 @@ int main(void)
 	{
 	for (y = 0; y <= 9; y++)
 	{
-	for (z = 0; z <= 9; z++)
+	for (z = '0'; z <= '9'; z++)
 	{
-	putchar('0' + x);
-	putchar('0' + y);
-	putchar('0' + z);
-	if (x != 7 || y != 8 || z != 9)
+	if (x < y && y < z)
+	{
+	putchar(x);
+	putchar(y);
+	putchar(z);
+
+	if (x != 7)
 	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	}
