3557a13ef217f0b86195ed168b3aca32800feeff
Divine Abazie <divineabazie14@gmail.com>
2022-10-17 05:42:53 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,20 +10,28 @@ int main(void)
 {
 	int x;
 	int y;
+	int n;
+	int m;
 
 	for (x = 0; x <= 9; ++y)
 	{
-	for (y = 0; y <= 9; ++y)
+	for (y = 0; y <= 8; ++y)
 	{
-	if (x < y)
+	for (n = 0; n <= 9; ++n)
+	{
+	for (m = 0; m <= 9; ++m)
+
+	if (x < y && y < n && n < m)
 	{
 	putchar('0' + x);
 	putchar('0' + y);
-
-	if (x != 9 || y != 9)
+	putchar('0' + n);
+	putchar('0' + m);
+	if (x != 9 || y != 8 || n != 9 || m != 9)
 	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	}
