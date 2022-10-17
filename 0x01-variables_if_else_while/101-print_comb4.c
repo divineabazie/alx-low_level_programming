318fc2fe4960aef5135e3de9033c9cbb5b422c5f
Divine Abazie <divineabazie14@gmail.com>
2022-10-17 01:55:32 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 9; x++)
+	{
+	for (y = 0; y <= 9; y++)
+	{
+	for (z = 0; z <= 9; z++)
+	{
+	putchar('0' + x);
+	putchar('0' + y);
+	putchar('0' + z);
+	if (x != 7 || y != 8 || z != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+
