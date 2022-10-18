6ac257e5cf01773a4936c42e9df3dcfcb461d38b
Divine Abazie <divineabazie14@gmail.com>
2022-10-18 10:28:58 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,17 +7,14 @@
  * Return: Always(0)
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
 	{
-	putchar(x);
+	_putchar(x);
 	}
 		_putchar('\n');
-
-	return (0);
-
 }
 
