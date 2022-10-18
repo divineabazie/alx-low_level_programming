cdff5233e3daeb402b8da8088409391db7d7175a
Divine Abazie <divineabazie14@gmail.com>
2022-10-18 04:23:41 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = "_putchar")
+	{
+	putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+
+
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabets
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+
+	putchar(x);
+
+		putchar('\n');
+	return (0);
+
+}
+
