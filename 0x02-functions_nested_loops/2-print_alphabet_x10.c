35f8ed9d1f3bea1d0d08c3a575b809d79447a429
Divine Abazie <divineabazie14@gmail.com>
2022-10-18 12:28:04 -0700

    lower alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints 10 times the alphabet in lowercase.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void);
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+	for (y = 'a'; y <= 'z'; y++)
+		_putchar(y);
+	}
+
+	_putchar('\n')
+
+
+
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase characters
+ *
+ */
+
+_islower(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	if ("c is lowercase")
+		_putchar(c);
+	}
+	return (0);
+	
+	{
+	else if ("c is otherwise")
+
+		_putchar(c); 
+	}
+		return (1);
+	
+
+}
