e34ed571c6155bb47cdfee8e50e11be3a3e339f9
Divine Abazie <divineabazie14@gmail.com>
2022-10-18 14:05:51 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: c
+ *
+ * Return: int
+ */
+
+int _isalpha(int c)
+{
+	int x;
+	
+	if (c = 'a' || c >= 'a' || c <= 'a' )
+		x = 1;
+	else
+		x = 0;
+
+	return (x);
+
+}
