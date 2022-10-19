a111c2df718219f3571de864fd93385071a06c37
Divine Abazie <divineabazie14@gmail.com>
2022-10-19 05:34:05 -0700

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,14 +12,14 @@ void times_table(void)
 	int num;
 	int count;
 
-	for(num = '0'; num <= 9; num++)
+	for (num = '0'; num <= 9; num++)
 	{
-	for(count = '0'; count <= '9'; count++)
+	for (count = '0'; count <= '9'; count++)
 	{
-		if (num < count)
-			_putchar(num = count * num)
+		if (num <= count)
+			_putchar(num = count * num);
 
-	_putchar(count)
+	_putchar(count);
 
 	_putchar('\n');
 	}
