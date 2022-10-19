26d28c2cb050df7f50a58f521e7781e306ad3357
Divine Abazie <divineabazie14@gmail.com>
2022-10-18 21:53:06 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,9 @@ void times_table(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 9; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		for (y = '0'; y <= '9'; y++)
 			_putchar(y);
 				_putchar('\n');
 	}
