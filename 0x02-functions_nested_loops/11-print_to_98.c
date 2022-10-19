27cedb2bfeb20ff06a8ef27aa50933ef3332cc06
Divine Abazie <divineabazie14@gmail.com>
2022-10-19 07:30:08 -0700

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,21 @@
 void print_to_98(int n)
 {
 	int x;
-	for (x < 0; x <= 98; x++}
+
+	for (x < 0; x <= 98; x++)
 	{
 	for (n < 0; n <= 98; n++)
 	{
 	if (n != 98)
 	{
-	_putchar(' ')
-	_putchar(',')
-	_putchar('\n')
+	_putchar(' ');
+	_putchar(',');
+	}
+	_putchar('\n');
 	}
 	}
-	}
+
+
+
+
 }
