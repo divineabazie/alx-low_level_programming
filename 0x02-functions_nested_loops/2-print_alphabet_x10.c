f06979e916a9c81e5869e07d232ec343fe404ad1
Divine Abazie <divineabazie14@gmail.com>
2022-10-19 03:10:56 -0700

    lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10(void) - prints 10 times the alphabet in lowercase.
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase.
  *
  * Return: Always 0
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int x, y;
 
