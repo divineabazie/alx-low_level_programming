99323353c779732a7f5589fcc62d47cf70374777
Divine Abazie <divineabazie14@gmail.com>
2022-10-18 22:49:42 -0700

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>                                               
+#include "main.h"                   
+
+/**
+ * print_last_digit - reutn value of last digit
+ *
+ * Return: int
+ */
+
+int print_last_digit(int x)
+{
+	int y;
+
+	if (x > 10)
+		y = x % 10;
+	
+	else 
+		y = x;
+	return (y);
+
+
+
+}
