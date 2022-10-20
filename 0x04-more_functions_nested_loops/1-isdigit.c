30a81b5309d065971fee025ea6ee4eedd2f65e8c
Divine Abazie <divineabazie14@gmail.com>
2022-10-20 06:29:17 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ *
+ * @c: c
+ * return: int
+ */
+
+int _isupper(int c)
+{
+	int x;
+
+	if (c >= 'A' && c <= 'Z')
+		x = 1;
+
+	else
+		x = 0;
+
+
+
+}
