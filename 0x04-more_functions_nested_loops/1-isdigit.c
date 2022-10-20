2d753fe02a6f62cd909ec24b7c2762a88e75f704
Divine Abazie <divineabazie14@gmail.com>
2022-10-20 10:53:06 -0700

    more numbers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -12,14 +12,10 @@ int _isdigit(int c)
 {
 	int x;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-	if (c != '9')
+	if (c = 0 && c <= 9)
 		x = 1;
-
 	else
 		x = 0;
-	}
 
 	return (x);
 
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	_putchar(y);
+	_putchar('\n');
+	}
+
+}
+
