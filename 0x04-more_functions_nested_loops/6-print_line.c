cf828298041d20bd2bdce3b32fd5dce0bfd698d1
Divine Abazie <divineabazie14@gmail.com>
2022-10-20 13:42:32 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal
+ *
+ * @n: is the new line
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+
+	int x;
+
+	x = 0;
+	if (n >= 0)
+	{
+	while (x <= n)
+	{
+		_putchar('_');
+		x++;
+	}
+	}
+	else
+	_putchar('\n');
+
+
+
+
+
+
+
+}
