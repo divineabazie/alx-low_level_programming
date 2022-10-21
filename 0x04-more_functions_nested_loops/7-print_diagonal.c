821c8d882da36a2961ce9d31613df5c6e2a551ec
Divine Abazie <divineabazie14@gmail.com>
2022-10-21 13:35:42 -0700

    diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,19 @@
 
 void print_diagonal(int n)
 {
-	int x;
+	int x = 0;
+	int y;
 
-	x = 0;
+	if (n <= 0)
+		_putchar('\n');
 
-	while (x < n)
+	for (x = 0; x < n; x++)
 	{
-		x++;
+	while (x++ < n)
+		_putchar(' ');
 		_putchar(92);
-	}
-
 	_putchar('\n');
-
+	}
 
 
 }
