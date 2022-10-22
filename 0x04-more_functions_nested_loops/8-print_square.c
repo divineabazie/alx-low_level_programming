92d9d8df31a5e558da201c34390298e23e978ae9
Divine Abazie <divineabazie14@gmail.com>
2022-10-22 07:50:55 -0700

    squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,17 +13,18 @@ void print_square(int size)
 	int x;
 	int y;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-	for (x = 1; x <= size; x++)
-	{
-	for (y = 1; y <= size; y++)
-	{
-	_putchar('#');
-	_putchar('\n');
-	}
-	}
-	}
-	else
 		_putchar('\n');
+	}
+	{
+		for (x = 0; x <= size; x++)
+	{
+		for (y = 0; y <= size; y++)
+			_putchar('#');
+	}
+	}
+		putchar('\n');
+
+
 }
