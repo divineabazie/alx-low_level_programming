d3a43eef24b40eeb644f6541a7857c61bd2ad870
Divine Abazie <divineabazie14@gmail.com>
2022-10-24 16:01:11 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * int _strlen - a function that returns the length of a string
+ *@s: string lenth declared
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+
+	length = strlen(s);
+
+	return (0);
+}
